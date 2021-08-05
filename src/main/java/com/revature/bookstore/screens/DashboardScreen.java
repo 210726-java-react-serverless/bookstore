c6f388f77610952eeaeebdd7c750da0e1dee72a3
jdsmith1906 <jdsmith1906@gmail.com>
2021-08-05 18:38:18 -0400

    .equals fix

--- src/main/java/com/revature/bookstore/screens/DashboardScreen.java
+++ src/main/java/com/revature/bookstore/screens/DashboardScreen.java
@@ -1,1 +1,43 @@
-    
+package com.revature.bookstore.screens;
+
+import com.revature.bookstore.repos.UserRepository;
+import com.revature.bookstore.util.ScreenRouter;
+
+import java.io.BufferedReader;
+
+
+public class DashboardScreen  extends Screen {
+
+    public DashboardScreen(BufferedReader consoleReader, ScreenRouter router) {
+        super("DashboardScreen", "/dashboard", consoleReader, router);
+    }
+    UserRepository user;
+    /*
+        TODO
+         Implement a dashboard that displays the user's username and gives them the option
+         to navigate to other screens (e.g. UserProfileScreen).
+     */
+    @Override
+    public void render() throws Exception {
+        //used functional printing to include username in the welcoming message
+       // System.out.printf("Welcome %n to the Dashboard Screen\n", user.dbUsername);
+        System.out.println("Here, you can review and update your details\n");
+        System.out.println("Here are your current details:\n");
+
+
+        System.out.println(user.dbUsername + "What task would you like to complete next:\n");
+        //Screen options that they can choose from
+        System.out.println("1- User Profile Screen");
+        System.out.println("2 -Welcome Screen");
+
+        String userInput = consoleReader.readLine();
+
+        if(userInput.equals("1")){
+            router.navigate("/profile");
+        } else if (userInput.equals("2")){
+            router.navigate("/welcome");
+        }
+        router.navigate("/welcome");
+    }
+
+}
