244b23d606df0c28e089555ad5ff54292794cc61
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-05 16:48:32 -0500

    Implemented a simple dashboard. This should connect to the UserProfile once and if it is completed.

--- src/main/java/com/revature/bookstore/screens/DashboardScreen.java
+++ src/main/java/com/revature/bookstore/screens/DashboardScreen.java
@@ -23,10 +23,29 @@ public class DashboardScreen  extends Screen {
      */
     @Override
     public void render() throws Exception {
-
+        // Receive a user from the UserService
         AppUser user = userService.receiveUser();
 
-        System.out.println(user.getUsername());
-    }
+        System.out.println("Welcome to the Dashboard, " + user.getUsername() + "!\n");
 
+        System.out.print("\nYou are currently logged in. What would you like to do?"
+                    + "\n1) Check or change your user profile"
+                    + "\n2) Logout"
+                    + "\n> ");
+
+        String input = consoleReader.readLine();
+
+        switch(input) {
+            case "1":
+                System.out.println("Understood. Navigating you to your user profile portal...");
+                router.navigate("/userprofile"); // TODO: Make this match the completed UserProfileScreen route
+                break;
+            case "2":
+                System.out.println("Understandable. Have a nice day!");
+                router.navigate("/welcome");
+                break;
+            default:
+                System.out.println("Sorry! We didn't quite catch that! Can you please try again?");
+        }
+    }
 }
