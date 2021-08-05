de6837735e0827df63557a7c929ca2654ac29547
Sean <seanprdunn@gmail.com>
2021-08-05 19:42:05 -0400

    Removed old code, cleaned up try-catches

--- src/main/java/com/revature/bookstore/repos/UserRepository.java
+++ src/main/java/com/revature/bookstore/repos/UserRepository.java
@@ -31,42 +31,16 @@ public class UserRepository implements CrudRepository<AppUser> {
     }
 
     public AppUser findUserByCredentials(String username, String password) {
-
-//        Properties appProperties = new Properties();
-//
-//        try {
-//            appProperties.load(new FileReader("src/main/resources/application.properties"));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            throw new ResourcePersistenceException("Unable to load properties file.");
-//        }
-//
-//        String ipAddress = appProperties.getProperty("ipAddress");
-//        int port = Integer.parseInt(appProperties.getProperty("port"));
-//        String dbName = appProperties.getProperty("dbName");
-//        String dbUsername = appProperties.getProperty("username");
-//        String dbPassword = appProperties.getProperty("password");
-//
-//        // TODO obfuscate DB credentials
-//        // TODO abstract connection logic from here
-//        try (MongoClient mongoClient = MongoClients.create(
-//                MongoClientSettings.builder()
-//                                   .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(ipAddress, port))))
-//                                   .credential(MongoCredential.createScramSha1Credential(dbUsername, dbName, dbPassword.toCharArray()))
-//                                   .build()
-//        )) {
-
-            try {
-//            MongoDatabase bookstoreDatabase = mongoClient.getDatabase("bookstore");
+        try {
             MongoDatabase bookstoreDatabase = getDBConnection();
 
             MongoCollection<Document> usersCollection = bookstoreDatabase.getCollection("users");
             Document queryDoc = new Document("username", username).append("password", password);
             Document authUserDoc = usersCollection.find(queryDoc).first();
 
-//            if (authUserDoc == null) {
-//                return null;
-//            }
+            if (authUserDoc == null) {
+                return null;
+            }
 
             ObjectMapper mapper = new ObjectMapper();
             AppUser authUser = mapper.readValue(authUserDoc.toJson(), AppUser.class);
@@ -94,33 +68,9 @@ public class UserRepository implements CrudRepository<AppUser> {
 
     @Override
     public AppUser save(AppUser newUser) {
-
-        Properties appProperties = new Properties();
-
         try {
-            appProperties.load(new FileReader("src/main/resources/application.properties"));
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ResourcePersistenceException("Unable to load properties file.");
-        }
-
-        String ipAddress = appProperties.getProperty("ipAddress");
-        int port = Integer.parseInt(appProperties.getProperty("port"));
-        String dbName = appProperties.getProperty("dbName");
-        String username = appProperties.getProperty("username");
-        String password = appProperties.getProperty("password");
-
-        // TODO obfuscate DB credentials
-        // TODO abstract connection logic from here
-        try (MongoClient mongoClient = MongoClients.create(
-                MongoClientSettings.builder()
-                                   .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(ipAddress, port))))
-                                   .credential(MongoCredential.createScramSha1Credential(username, dbName, password.toCharArray()))
-                                   .build()
-        )) {
-
-            MongoDatabase bookstoreDb = mongoClient.getDatabase("bookstore");
-            MongoCollection<Document> usersCollection = bookstoreDb.getCollection("users");
+            MongoDatabase bookstoreDatabase = getDBConnection();
+            MongoCollection<Document> usersCollection = bookstoreDatabase.getCollection("users");
             Document newUserDoc = new Document("firstName", newUser.getFirstName())
                                        .append("lastName", newUser.getLastName())
                                        .append("email", newUser.getEmail())
@@ -131,10 +81,12 @@ public class UserRepository implements CrudRepository<AppUser> {
             newUser.setId(newUserDoc.get("_id").toString());
             System.out.println(newUser);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
 
         return newUser;
-
     }
 
     @Override
