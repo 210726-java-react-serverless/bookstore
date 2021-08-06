f766025c27a076ceaec2ead6b8203e9c7a7fb731
wsingleton <wezley.singleton@gmail.com>
2021-08-06 11:37:46 -0400

    moved and renamed class that holds abstracted connection establishment logic

--- src/main/java/com/revature/bookstore/util/MongoClientFactory.java
+++ src/main/java/com/revature/bookstore/util/MongoClientFactory.java
@@ -1,63 +1,66 @@
-package com.revature.bookstore.services;
+package com.revature.bookstore.util;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.revature.bookstore.util.exceptions.ResourcePersistenceException;
+import com.revature.bookstore.util.exceptions.DataSourceException;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
-public class ConnectionFactory {
+public class MongoClientFactory {
 
-    private String ipAddress;
-    private String username;
-    private String dbName;
-    private String password;
-    private int port;
-    private MongoClient mongoClient;
-    private static ConnectionFactory connectionFactory = null;
+    private final MongoClient mongoClient;
+    private static final MongoClientFactory mongoClientFactory = new MongoClientFactory();
 
 
-    private ConnectionFactory(){
+    private MongoClientFactory() {
+
         Properties appProperties = new Properties();
 
         try {
             appProperties.load(new FileReader("src/main/resources/application.properties"));
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ResourcePersistenceException("Unable to load properties file.");
-        }
-        ipAddress = appProperties.getProperty("ipAddress");
-        port = Integer.parseInt(appProperties.getProperty("port"));
-        dbName = appProperties.getProperty("dbName");
-        username = appProperties.getProperty("username");
-        password = appProperties.getProperty("password");
 
-        try{
-            this.mongoClient = MongoClients.create(
-                    MongoClientSettings.builder()
-                            .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(ipAddress, port))))
-                            .credential(MongoCredential.createScramSha1Credential(username, dbName, password.toCharArray()))
-                            .build());
+            String ipAddress = appProperties.getProperty("ipAddress");
+            int port = Integer.parseInt(appProperties.getProperty("port"));
+            String dbName = appProperties.getProperty("dbName");
+            String username = appProperties.getProperty("username");
+            char[] password = appProperties.getProperty("password").toCharArray();
+
+            List<ServerAddress> hosts = Collections.singletonList(new ServerAddress(ipAddress, port));
+            MongoCredential credentials = MongoCredential.createScramSha1Credential(username, dbName, password);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                                                              .applyToClusterSettings(builder -> builder.hosts(hosts))
+                                                              .credential(credentials)
+                                                              .build();
+
+            this.mongoClient = MongoClients.create(settings);
+
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace(); // TODO log this to a file
+            throw new DataSourceException("Unable to load database properties file.", fnfe);
+        } catch(Exception e){
+            e.printStackTrace(); // TODO log this to a file
+            throw new DataSourceException("An unexpected exception occurred.", e);
         }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+
     }
+
     public void cleanUp(){
         mongoClient.close();
     }
-    public static ConnectionFactory getInstance(){
-        if(connectionFactory==null){
-            connectionFactory=new ConnectionFactory();
-        }
-        return connectionFactory;
+
+    public static MongoClientFactory getInstance(){
+        return mongoClientFactory;
     }
+
     public MongoClient getConnection(){
         return mongoClient;
     }
+
 }
