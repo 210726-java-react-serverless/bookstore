f764b9857a0366f997ffb6e313a0f975fdcafa81
wsingleton <wezley.singleton@gmail.com>
2021-08-09 22:02:52 -0400

    using provide MongoDB/Java PojoCodec for document <-> POJO mappings; removed Jackson logic from repo layer

--- src/main/java/com/revature/bookstore/datasource/util/MongoClientFactory.java
+++ src/main/java/com/revature/bookstore/datasource/util/MongoClientFactory.java
@@ -7,12 +7,18 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 import com.revature.bookstore.util.exceptions.DataSourceException;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class MongoClientFactory {
 
@@ -35,9 +41,14 @@ public class MongoClientFactory {
 
             List<ServerAddress> hosts = Collections.singletonList(new ServerAddress(ipAddress, port));
             MongoCredential credentials = MongoCredential.createScramSha1Credential(username, dbName, password);
+            CodecRegistry defaultCodecRegistry = getDefaultCodecRegistry();
+            PojoCodecProvider pojoCodecProvider= PojoCodecProvider.builder().automatic(true).build();
+            CodecRegistry pojoCodecRegistry = fromRegistries(defaultCodecRegistry, fromProviders(pojoCodecProvider));
+
             MongoClientSettings settings = MongoClientSettings.builder()
                                                               .applyToClusterSettings(builder -> builder.hosts(hosts))
                                                               .credential(credentials)
+                                                              .codecRegistry(pojoCodecRegistry)
                                                               .build();
 
             this.mongoClient = MongoClients.create(settings);
