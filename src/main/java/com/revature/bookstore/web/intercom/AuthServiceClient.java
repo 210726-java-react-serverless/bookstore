d2b90d34adf71e771cb967931f33ba300a26039c
wsingleton <wezley.singleton@gmail.com>
2021-09-07 21:42:09 -0400

    ignorning all boot application ymls

--- src/main/java/com/revature/bookstore/web/intercom/AuthServiceClient.java
+++ src/main/java/com/revature/bookstore/web/intercom/AuthServiceClient.java
@@ -2,6 +2,7 @@ package com.revature.bookstore.web.intercom;
 
 import com.revature.bookstore.web.dtos.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class AuthServiceClient {
 
     private final RestTemplate restClient;
+    @Value("${auth-endpoint}")
     private final String authServiceUrl = "http://authservice-env.eba-hmvam7bf.us-east-1.elasticbeanstalk.com/auth-service/token";
 
     @Autowired
